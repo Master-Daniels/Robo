ca0f5462e185ac9147e602d0bca2eafe0005783a
Master-Daniel <adebayooluwasegun011@gmail.com>
2022-03-17 16:09:11 +0100

    Updates

---
+++ precache-manifest.22e03415ea005f13987b2258cf5e28c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a83b21d354dc309f8f3",
+    "url": "/Robo/static/css/main.46584aca.chunk.css"
+  },
+  {
+    "revision": "7a83b21d354dc309f8f3",
+    "url": "/Robo/static/js/main.7a83b21d.chunk.js"
+  },
+  {
+    "revision": "4601723811cbd90b523d",
+    "url": "/Robo/static/css/1.9afc6cdb.chunk.css"
+  },
+  {
+    "revision": "4601723811cbd90b523d",
+    "url": "/Robo/static/js/1.46017238.chunk.js"
+  },
+  {
+    "revision": "ae9fdd3edbcecdf22fcd",
+    "url": "/Robo/static/js/runtime~main.ae9fdd3e.js"
+  },
+  {
+    "revision": "d2a560274fd599dc120a3550c9adde3e",
+    "url": "/Robo/static/media/Cyttah.d2a56027.otf"
+  },
+  {
+    "revision": "806e8fe2965b493ecbadeedd773f47d3",
+    "url": "/Robo/index.html"
+  }
+];
