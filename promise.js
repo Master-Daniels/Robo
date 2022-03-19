80b0d966cc5f5467aaf192a81dc1cf5d92add111
Master-Daniel <adebayooluwasegun011@gmail.com>
2022-03-19 01:10:39 +0100

    Promise....

---
+++ promise.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import 'tachyons';
+
+
+
+export default Promise
--- src/index.js
+++ src/index.js
@@ -1,11 +1,81 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
-import "./index.css";
+// import "./index.css";
+import "tachyons";
+
+class Promise extends Component {
+  constructor() {
+    super();
+    this.state = {
+      color: "bg-white",
+    };
+  }
+  colorChange = (color) => {
+    const innerFunc = () => {
+      this.setState({ color });
+    };
+    return innerFunc;
+  };
+  changeBack = () => {
+    this.setState({ color: "bg-white" });
+  };
+  render() {
+    return (
+      <div>
+        <article class={`center mw5 mw6-ns hidden ba mv4  ${this.state.color}`}>
+          <h1 class="f4 bg-orange white mv0 pv2 ph3">Title of card</h1>
+          <div class="pa3 bt">
+            <p class="f6 f5-ns lh-copy measure mv0">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum.
+            </p>
+          </div>
+          <div>
+            <a
+              onMouseEnter={this.colorChange("bg-blue")}
+              onMouseLeave={this.changeBack}
+              class="f6 grow no-underline br-pill ph4 pv2 mb2 dib white bg-blue ma2"
+              href="#0"
+            >
+              Blue
+            </a>
+            <a
+              onMouseEnter={this.colorChange("bg-yellow")}
+              onMouseLeave={this.changeBack}
+              class="f6 grow no-underline br-pill ph4 pv2 mb2 dib white bg-yellow ma2"
+              href="#0"
+            >
+              Yellow
+            </a>
+            <a
+              onMouseEnter={this.colorChange("bg-green")}
+              onMouseLeave={this.changeBack}
+              class="f6 grow no-underline br-pill ph4 pv2 mb2 dib white bg-green ma2"
+              href="#0"
+            >
+              Green
+            </a>
+            <a
+              onMouseEnter={this.colorChange("bg-gray")}
+              onMouseLeave={this.changeBack}
+              class="f6 grow no-underline br-pill ph4 pv2 mb2 dib white bg-gray ma2"
+              href="#0"
+            >
+              Gray
+            </a>
+          </div>
+        </article>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <>
-    <App />
+    <Promise />
   </>,
   document.getElementById("root")
 );
